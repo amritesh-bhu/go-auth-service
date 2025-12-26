e8fb08218416ee5700a10fd8f23f6718db6d6833
amritesh-bhu <amriteshbhu438@gmail.com>
2025-12-26 12:41:12 +0530

    login credential in progress

---
+++ src/domain/auth.go
@@ -0,0 +1,68 @@
+package domain
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/go-auth-service/src/entities"
+	"github.com/go-auth-service/src/helper"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type LoginRequest struct {
+	EmailId  string `bson:"emailId" json:"emailId"`
+	Password string `bson:"password" json:"password"`
+}
+
+type RegisterResponse struct {
+	ID        primitive.ObjectID `json:"id"`
+	EmailId   string             `json:"emailId"`
+	FirstName string             `json:"firstName"`
+	LastName  string             `json:"lastName"`
+}
+
+var UsersCollection *mongo.Collection
+
+func RegisterUser(ctx context.Context, user *entities.User) (*RegisterResponse, error) {
+	ctx, cancel := context.WithTimeout(context.TODO(), 5*time.Second)
+	defer cancel()
+
+	hashedPassword, err := helper.HashPassword(user.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	user.Password = hashedPassword
+
+	result, err := UsersCollection.InsertOne(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+
+	// assign generated _id back to user
+	response := &RegisterResponse{
+		ID:        result.InsertedID.(primitive.ObjectID),
+		EmailId:   user.EmailId,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+	}
+
+	return response, nil
+}
+
+func LoginUser(ctx context.Context, cred *LoginRequest) (string, error) {
+
+	var user entities.User
+	if err := UsersCollection.FindOne(ctx, bson.M{"emailId": cred.EmailId}).Decode(&user); err != nil {
+		return "", errors.New("User does not exist!")
+	}
+
+	if helper.ComparePassword(user.Password, cred.Password) {
+		return "", errors.New("Invalid credentials!")
+	}
+
+	return "Logged in successfully!", nil
+}
